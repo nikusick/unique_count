29bff3e5ce7e38769691af3baf3380e2aa241462
veronika_sibova <nika.sibova.03@inbox.ru>
2022-09-13 15:01:13 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, char** argv) {
+
+    std::ifstream fin(argv[1]);
+    std::ofstream fout(argv[2]);
+
+    std::vector<int> nums;
+    std::string num;
+
+    while (fin >> num) {
+        nums.push_back(stoi(num));
+    }
+
+    auto unique_nums = std::unique(nums.begin(), nums.end());
+    nums.erase(unique_nums, nums.end());
+
+    auto one_count = std::count(nums.begin(), nums.end(), 1);
+    auto zero_count = std::count(nums.begin(), nums.end(), 0);
+    fout << one_count << " "  << zero_count;
+
+    fin.close();
+    fout.close();
+    return 0;
+}
