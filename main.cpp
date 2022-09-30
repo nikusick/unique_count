13255fdceae3ef658bd3ad6462c8f63d97e0f903
veronika_sibova <nika.sibova.03@inbox.ru>
2022-09-30 21:19:13 +0300

    add arguments test

--- main.cpp
+++ main.cpp
@@ -5,25 +5,25 @@
 
 int main(int argc, char** argv) {
     if (argc == 3) {
-    std::ifstream fin(argv[1]);
-    std::ofstream fout(argv[2]);
+        std::ifstream fin(argv[1]);
+        std::ofstream fout(argv[2]);
 
-    std::vector<int> nums;
-    int num;
+        std::vector<int> nums;
+        int num;
 
-    while (fin >> num) {
-        nums.push_back(num);
-    }
+        while (fin >> num) {
+            nums.push_back(num);
+        }
 
-    auto uniqueNums = std::unique(nums.begin(), nums.end());
-    nums.erase(uniqueNums, nums.end());
+        auto uniqueNums = std::unique(nums.begin(), nums.end());
+        nums.erase(uniqueNums, nums.end());
 
-    auto unityCount = std::count(nums.begin(), nums.end(), 1);
-    auto zeroCount = std::count(nums.begin(), nums.end(), 0);
-    fout << unityCount << " "  << zeroCount;
+        auto unityCount = std::count(nums.begin(), nums.end(), 1);
+        auto zeroCount = std::count(nums.begin(), nums.end(), 0);
+        fout << unityCount << " "  << zeroCount;
 
-    fin.close();
-    fout.close();
+        fin.close();
+        fout.close();
     }
     else {
         std::cout << "Must be 2 arguments: input and output files";
